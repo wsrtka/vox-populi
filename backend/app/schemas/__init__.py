0fb887e96ce9f9b0a3fc640e7078cd8757fd499f
wsrtka <witold.serwatka@pm.me>
2025-08-24 22:21:22 +0200

    add current term

---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,5 @@
+from mp import MP
+from party import Party
+from term import Term
+from vote import Vote
+from vote_cast import VoteDecision
--- backend/app/utils/sejm_api.py
+++ backend/app/utils/sejm_api.py
@@ -1,12 +1,20 @@
 import requests
+from sqlalchemy.orm import Session
 
 from ..config import settings
+from ..schemas import Term
 
 
 BASE_URL = settings.SEJM_API_BASE.rstrip('/')
 
 
-def get_json(endpoint):
+def get_current_term(db: Session):
+    current_term = db.query(Term).filter_by(current=True).first()
+    global BASE_URL
+    if current_term:
+        BASE_URL = f"{BASE_URL}/term{current_term.id}"
+
+def get_json(endpoint: str):
     url = f"{BASE_URL}/{endpoint.lstrip('/')}"
     res = requests.get(url)
     res.raise_for_status()
